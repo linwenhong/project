7020efa741b04c52357687f102ec913d69dd34ba
Administra <Administra>
2017-10-25 15:19:27 +0800

    餐区管理-添加移台清台

--- src/app/home/table/table.component.ts
+++ src/app/home/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }      from '@angular/router';
 import { HomeService } from '../home.service';
 
 @Component({
@@ -11,7 +12,9 @@ export class TableComponent implements OnInit {
 	public select_desk: any;
 	public shield: boolean = false;
 	public peoples: any = null;
-  constructor(private service: HomeService) { }
+	private isMove: boolean = false;
+	private last_id: any;
+  constructor(private service: HomeService, private router: Router) { }
 
   ngOnInit() {
 		this.getshop();
@@ -28,12 +31,17 @@ export class TableComponent implements OnInit {
 	}
 	
 	select(data: any): void {
-		console.log(data);
-		this.select_desk = data;
+		if(this.isMove){
+			this.isMove = false;
+			this.toMove(data);
+		}else{
+			this.select_desk = data;
+		}
 	}
 	//开台
 	open(): void {
 		this.shield = true;
+		console.log(this.select_desk);
 	}
 	//取消
 	hide(): void {
@@ -42,6 +50,69 @@ export class TableComponent implements OnInit {
 	//确定开台
 	confirm(): void {
 		this.shield = false;
-		console.log(this.peoples);
+		this.desk_status(1, this.peoples);
+	}
+	//选择移台
+	move(): void {
+		this.isMove = true;
+		this.last_id = this.select_desk.table_id;
+	}
+	//移台
+	toMove(data: any): void {
+		let request = {
+			shop_id: localStorage.getItem('shopId'),
+			tableCode: this.last_id
+		}
+		request['newCode'] = data.table_id;
+		this.service.post('bk_update_order_desk', request).then(
+			res => {
+				this.select_desk = {};
+     		this.select_desk.status = -1;
+     		
+	     	if(res.status == '200'){
+	     		this.getshop();
+	     	}else{
+	     		this.router.navigate(['/login']);
+	     	};
+	    }
+		);
+	}
+	//清台
+	clear(): void {
+		let request = {
+			shop_id: localStorage.getItem('shopId'),
+			tableCode: this.select_desk.table_id
+		}
+		this.service.post('bk_clear_desk', request).then(
+			res => {
+	     	this.select_desk = {};
+     		this.select_desk.status = -1;
+     		
+	     	if(res.status == '200'){
+	     		this.getshop();
+	     	}
+	    }
+		);
+	}
+	//修改桌子状态
+	desk_status(status: number, peoples: any): void {
+		let request = {
+  		status: status,
+  		tableCode: this.select_desk.table_id
+  	};
+		if(peoples) request['people'] = peoples;
+  	console.log(request);
+		this.service.post('bk_update_desk', request).then(
+			res => {
+	     	this.select_desk = {};
+     		this.select_desk.status = -1;
+     		
+	     	if(res.status == '200'){
+	     		this.getshop();
+	     	}else{
+	     		this.router.navigate(['/login']);
+	     	};
+	    }
+		);
 	}
 }
