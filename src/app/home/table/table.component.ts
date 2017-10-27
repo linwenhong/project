6b45ef746c11d69383f37a7389ad9de949d3ec0b
Administra <Administra>
2017-10-27 18:30:58 +0800

    添加清台确认

--- src/app/home/table/table.component.ts
+++ src/app/home/table/table.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }      from '@angular/router';
 import { HomeService } from '../home.service';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-table',
@@ -11,10 +14,12 @@ export class TableComponent implements OnInit {
 	public desk: any;
 	public select_desk: any;
 	public shield: boolean = false;
+	public toClear: boolean = false;
 	public peoples: any = null;
+	public password: any = null;
 	private isMove: boolean = false;
 	private last: any;
-  constructor(private service: HomeService, private router: Router) { }
+  constructor(private service: HomeService, private router: Router, private http: Http) { }
 
   ngOnInit() {
 		this.getshop();
@@ -54,6 +59,7 @@ export class TableComponent implements OnInit {
 	//取消开台
 	hide(): void {
 		this.shield = false;
+		this.toClear = false;
 		this.select_desk = {};
   	this.select_desk.status = -1;
 	}
@@ -73,14 +79,14 @@ export class TableComponent implements OnInit {
 				}).then(
 					res => {
 						this.shield = false;
-						this.select_desk = {};
-		     		this.select_desk.status = -1;
-		     		
 			     	if(res.status == '200'){
 			     		this.getshop();
+			     		notify('success', '更改人数', this.select_desk.table_num+'更改人数成功!');
 			     	}else{
-			     		this.router.navigate(['/login']);
+			     		notify('error', '更改人数', res.msg);
 			     	};
+						this.select_desk = {};
+		     		this.select_desk.status = -1;
 			    }
 				);
 			}
@@ -95,12 +101,8 @@ export class TableComponent implements OnInit {
 	toMove(data: any): void {
 		let request = {
 			shop_id: localStorage.getItem('shopId'),
-			tableCode: data.table_id
-		}
-		if(this.last.out_trade_no){
-			request['out_trade_no'] = this.last.out_trade_no;
-		}else{
-			request['oldCode'] = this.last.table_id;
+			tableCode: data.table_id,
+			oldCode: this.last.table_id
 		}
 		
 		this.service.post('bk_update_order_desk', request).then(
@@ -117,8 +119,8 @@ export class TableComponent implements OnInit {
 	    }
 		);
 	}
-	//清台
-	clear(): void {
+	//确认清台
+	private confirm_clear(): void {
 		let request = {
 			shop_id: localStorage.getItem('shopId'),
 			tableCode: this.select_desk.table_id
@@ -127,12 +129,40 @@ export class TableComponent implements OnInit {
 			res => {
 	     	if(res.status == '200'){
 	     		this.getshop();
+	     		this.toClear = false;
 	     		notify('success', '清台', this.select_desk.table_num+'清台成功!');
 	     	}else{
 	     		notify('error', '清台', res.msg);
 	     	};
 	     	this.select_desk = {};
      		this.select_desk.status = -1;
+	    }
+		);
+	}
+	//清台
+	clear(): void {
+		//已下单或结账
+		if(this.select_desk.status>=2){
+			this.toClear = true;
+		}else{
+			this.confirm_clear();
+		}
+	}
+	//清台输密
+	clear_table(): void {
+		this.http.post(environment.api_url+'bk_login',{
+			username: localStorage.getItem('username'),
+			password: this.password
+		}).toPromise().then(
+			response => {
+				let regions = response.json();
+	     	if(regions.code == '200'){
+	     		console.log(response.headers.get('Authorization'));
+	     		sessionStorage.setItem('token', regions.token);
+	     		this.confirm_clear();
+	     	}else{
+	     		notify('error', '输入错误', '请输入正确的密码!');
+	     	};
 	    }
 		);
 	}
@@ -150,6 +180,23 @@ export class TableComponent implements OnInit {
 	     		notify('success', '开台', this.select_desk.table_num+'开台成功!');
 	     	}else{
 	     		notify('error', '开台', res.msg);
+	     	};
+	     	this.select_desk = {};
+     		this.select_desk.status = -1;
+	    }
+		);
+	}
+	//订单详情
+	details(): void {
+		this.service.post('bk_getorders', {
+			shop_id: 1,
+			tableCode: this.select_desk.table_id
+		}).then(
+			res => {
+	     	if(res.status == '200'){
+	     		
+	     	}else{
+	     		notify('error', '获取订单失败', res.msg);
 	     	};
 	     	this.select_desk = {};
      		this.select_desk.status = -1;
