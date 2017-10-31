5f8a382f6d8d6f3adc86c7b8803f6b579a547225
Administra <Administra>
2017-10-31 17:02:54 +0800

    添加在线订单页面

--- src/app/home/order/order.component.ts
+++ src/app/home/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home.service';
 
 @Component({
   selector: 'app-order',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class OrderComponent implements OnInit {
 	isAutomatic: boolean = false;					//自动接单
 	isBusiness: boolean = false;					//营业状态
-  constructor() { }
+	select_index: number;
+	type: number = 1;
+	select_order: any = {};
+	orders: any;
+	order: any;
+  constructor(private service: HomeService) { }
 
   ngOnInit() {
+  	this.service.post('bk_online_orders', {
+  		shop_id: localStorage.getItem('shopId')
+  	}).then(
+			res => {
+	     	if(res.status == '200'){
+	     		this.orders = res.orders;   		
+	     		this.order = this.orders.new_order;   		
+	     	};
+	    }
+		);
   }
-
+	
+	select(index: number, data: any): void {
+		this.select_index = index;
+		this.select_order = data;
+		console.log(data);
+	}
+	
+	select_type(index: number, type: any): void {
+		this.type = index;
+		this.order = this.orders[type];
+		console.log(this.order);
+	}
 }
