c1086400802f9cf58ebfddce28e3762e0fdece18
Administra <Administra>
2017-10-31 13:54:39 +0800

    update

--- src/app/home/order/order.component.ts
+++ src/app/home/order/order.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-
+	isAutomatic: boolean = false;					//自动接单
+	isBusiness: boolean = false;					//营业状态
   constructor() { }
 
   ngOnInit() {
