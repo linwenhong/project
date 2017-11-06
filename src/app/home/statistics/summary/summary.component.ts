bfff89a952f6057f67a7cebd5e185ee4d7a56a0a
Administra <Administra>
2017-11-06 09:30:39 +0800

    添加统计汇总页面

--- src/app/home/statistics/summary/summary.component.ts
+++ src/app/home/statistics/summary/summary.component.ts
@@ -11,16 +11,33 @@ export class SummaryComponent implements OnInit {
 	start_time: any;
 	end_time: any;
 	condition: number;
+	datas: any;
+	total: number = 0;
+	
   constructor(public service: HomeService, public statisticsService: StatisticsService) {
   	statisticsService.select_nav = 2;
   }
 
   ngOnInit() {
+  	this.search(null, null);
   }
   
 	search(start: any, end: any): void {
-		console.log(start, end);
-		//todo
-		
+		this.service.post('bk_get_totalreport', {
+  		shop_id: localStorage.getItem('shopId'),
+  		start_time: start,
+  		end_time: end
+  	}).then(
+			res => {
+	     	if(res.status == '200'){
+	     		this.datas = res.orders;
+	     		for(let x in this.datas){
+	     			this.total += this.datas[x].num;
+	     		}
+	     	}else{
+	     		notify('error', '错误', res.msg);
+	     	};
+	    }
+		);
 	}
 }
