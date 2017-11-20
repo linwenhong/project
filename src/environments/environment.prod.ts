bd4cde9967438a68e0282e3002002ea6e3af6bdf
Administra <Administra>
2017-11-20 17:31:19 +0800

    添加test api配置文件

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-//api_url: "http://api.baixr.com/api/"
-	api_url: "http://api.baixingrong.test.ywqian.com/api/"
+	api_url: "http://api.baixr.com/api/"
 };
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+	api_url: "http://api.baixingrong.test.ywqian.com/api/"
+};
