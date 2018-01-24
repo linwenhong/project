9da5023932ce1e1f8ae8dc6cabdc493042129f40
Administra <Administra>
2018-01-24 09:29:41 +0800

    添加合同对象

--- src/app/common/contract.ts
+++ src/app/common/contract.ts
@@ -1,14 +1,18 @@
 export class Contract {
-  name: string;
-  project: number;              /*项目id*/
-  type: number;                 /*报告类型id*/
-  format: number;               /*报告格式id*/
-  record_number: string;        /*样品/记录编号*/
-  blind_sample_number: string;  /*盲样编号*/
-  element: string;              /*要素*/
-
-  person_in_charge: number;     /*负责人*/
-
-  id: number;
-  create_time: number;
+  project_name: string;   /*工程名称*/
+  contract_num: string;   /*合同编号*/
+  party_a: string;   /*甲方名称*/
+  sign_date: string;   /*签订日期*/
+  limit_time: string;   /*工期*/
+  finish_time: string;   /*预计完成时间*/
+  entry_time: string;   /*要求进场时间*/
+  contract_amount: string;   /*合同金额*/
+  contract_files: string;   /*合同文件*/
+  status: string;   /*合同状态*/
+  dp_id: string;   /*负责部门*/
+  people: string;   /*负责人*/
+  contract_type: string;   /*合同类型*/
+  contract_item: string;   /*检测项目名称*/
+  contract_params: string;   /*检测项目参数*/
+  audit: string;   /*备注*/
 }
