c549401848a1e215a7397f7e27d536e348bbed04
Administra <Administra>
2018-01-25 10:44:32 +0800

    update

--- src/app/home/my/my.component.ts
+++ src/app/home/my/my.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
 import { Router } from '@angular/router';
 
@@ -11,85 +10,21 @@ import { User } from '../../common/user';
   styleUrls: ['./my.component.css', '../../../assets/form.css']
 })
 export class MyComponent implements OnInit {
-  Form: FormGroup;
-  isSubmit: boolean;
-  FormKeys: string[] = [
-    'username',
-    'name',
-    'dp_id',
-    'avatar',
-    'sign_img',
-  ];
   user: User;
   isShowSignature: boolean;
 
   constructor(
     private router: Router,
-    private http: Http,
-    private  fb: FormBuilder
-  ) {
-    this.createForm();
-  }
+    private http: Http
+  ) { }
 
   ngOnInit() {
-    this.isSubmit = false;
     this.isShowSignature = false;
-    const FormCache = JSON.parse(localStorage.getItem('my'));
-    if (FormCache) {
-      this.setPatchValue(this.Form, FormCache);
-      this.user = FormCache;
-    }
-  }
-
-  createForm(): void {
-    this.Form = this.fb.group({
-      username: ['', Validators.required],
-      name: ['', Validators.required],
-      dp_id: ['', Validators.required],
-      avatar: [''],
-      sign_img: [''],
-    });
+    this.user = JSON.parse(localStorage.getItem('my'));
   }
 
   showSignature(): void {
     this.isShowSignature = !this.isShowSignature;
-  }
-
-  getFormValue(form: FormGroup): User {
-    const formValue = new User();
-    this.FormKeys.forEach(key => {
-      formValue[key] = form.get(key).value;
-    });
-    return formValue;
-  }
-
-  setPatchValue(form: FormGroup, patchValue: User): void {
-    form.patchValue(patchValue);
-  }
-
-  submit(form: FormGroup): void {
-    this.isSubmit = true;
-    if (form.status === 'INVALID') {
-      muiToast('请完善提交信息');
-      return;
-    }
-    const request = this.getFormValue(form);
-    this.setPatchValue(form, request);
-    console.log(request);
-    /**
-     *TODO:提交项目申请表数据 => 跳转页面
-     *simulation：模拟方法(保存提交数据)
-     **/
-    this.simulation(request);
-  }
-
-  simulation(user: User): void {
-    const time = new Date().getTime();
-    user.id = time;
-
-    localStorage.setItem('my', JSON.stringify(user));
-    muiToast('修改成功');
-    this.isSubmit = false;
   }
 
   logout(): void {
